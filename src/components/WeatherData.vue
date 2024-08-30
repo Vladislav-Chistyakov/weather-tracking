556b03c42653b40c2ab75dcad90bed48e35ed341
Vladislav_Chistyakov <vladischis@gmail.com>
2024-08-30 16:30:03 +0300

    Удалось узнать погоду по цельсию

--- src/components/WeatherData.vue
+++ src/components/WeatherData.vue
@@ -7,14 +7,19 @@ import {onMounted, ref} from 'vue'
 const lat = '58.000'
 const lon = '56.316'
 const cnt = '7'
+
 const apiKey = '13b55ccc56185568d11b8e020229a105'
-const test = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`
-const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+const apiPro = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
 
 // const { data, error } = useFetch(test)
 
 
 onMounted(async () => {
+  // TODO Работает только с VPN
+  const time = new Date().getTime()
+  console.warn('1111', api)
+  const test = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${apiKey}`
   await fetch(api)
       .then((res) => console.warn('res', res))
       .then((json) => (data.value = json))
